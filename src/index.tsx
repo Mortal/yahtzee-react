4a9114f6ae6048729c4fbfdd0e9ef08abbb8722c
Mathias Rav <m@git.strova.dk>
2019-04-19 23:21:23 +0200

    Yahtzee WIP

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {observable, computed} from "mobx";
+import {observable, computed, action} from "mobx";
 import {observer} from "mobx-react";
 import styles from "./index.scss";
 
 class App {
 	@observable
-	foo = "";
+	players = [];
+
+	@action
+	onPlayerCountChange(i) {
+		while (this.players.length > i) this.players.pop();
+		while (this.players.length < i) {
+			const r = [];
+			for (let j = 0; j < 20; ++j) r.push(0);
+			this.players.push(r);
+		}
+	}
 }
 
 const app = new App();
@@ -14,9 +24,43 @@ const app = new App();
 @observer
 class AppComponent extends React.Component<{}, {}> {
 	render() {
+		const header = <>
+			<div className={styles.ScoreHeader}>
+			<div>Enere</div>
+			<div>Toere</div>
+			<div>Treere</div>
+			<div>Firere</div>
+			<div>Femmere</div>
+			<div>Seksere</div>
+			<div>Sum</div>
+			<div>Bonus ved 84 eller flere</div>
+			<div>1 par</div>
+			<div>2 par</div>
+			<div>3 par</div>
+			<div>3 ens</div>
+			<div>4 ens</div>
+			<div>2 x 3 ens</div>
+			<div>Lav 1-2-3-4-5</div>
+			<div>Høj 2-3-4-5-6</div>
+			<div>Cameron 1-2-3-4-5-6</div>
+			<div>Fuldt hus 3 + 2 ens</div>
+			<div>Chance</div>
+			<div>Super-yatzy</div>
+			</div>
+		</>;
+
+		const players = app.players.map((v, i) =>
+			<div key={i} className={styles.ScoreColumn}>
+			{v.map((c, j) => <div><input value={c} onChange={e => v[j] = e.target.value} /></div>)}
+			</div>
+		);
+
 		return <>
-			<div><input value={app.foo} onChange={e => app.foo = e.target.value} /></div>
-			<div>The value is <span className={styles.MyTestStyle}>{app.foo}</span></div>
+			<div>Players: <input value={app.players.length} onChange={e => app.onPlayerCountChange(parseInt(e.target.value))} /></div>
+			<div className={styles.ScoreTable}>
+			{header}
+			{players}
+			</div>
 		</>;
 	}
 }
